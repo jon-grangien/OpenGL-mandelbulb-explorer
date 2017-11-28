1e443d6cd3fed72da91aac172e4394b36e78a6e4
codingInSpace <jonathan.grangien@gmail.com>
2017-11-28 22:08:49 +0100

    Bump GLFW and GLSL versions

--- shaders/mandel_raymarch.frag
+++ shaders/mandel_raymarch.frag
@@ -1,4 +1,4 @@
-#version 330 core
+#version 400 core
 
 in vec3 vertRayOrigin;
 in vec3 vertRayDirection;
--- src/Window.cc
+++ src/Window.cc
@@ -19,7 +19,7 @@ bool Window::init(FrameBufferSizeCallback resizeCallback,
 
   glfwSetErrorCallback(error_callback);
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
